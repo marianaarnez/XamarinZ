85fde77e160e86bc0f57791ea17bf5b5e793b363
Mariana Arnez <mariana.anez@gmail.com>
2019-12-29 20:20:05 -0400

    Agregar archivos de proyecto.

---
+++ App/App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ViewModels
+{
+    class LoginViewModel:BaseViewModel
+    {
+        #region variables
+        string email;
+        string password;
+        bool isrunning;
+
+        #endregion
+        #region propiedades
+        public string Email {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        #endregion
+    }
+}
